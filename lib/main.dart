982e145cba9f189aa82931c383ee1a238e5ce164
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-04-29 10:26:13 -0300

    atualizando main e yaml com novas imagens

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,31 +6,20 @@ void main() {
 }
 
 class Bytebank extends StatelessWidget {
-  final String nomeApp = "Bytebank";
+  final String nomeApp = "Dashboard";
   const Bytebank({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: nomeApp,
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: const TelaPrincipal(),
-    );
-  }
-}
-
-class TelaPrincipal extends StatelessWidget {
-  const TelaPrincipal({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Bytebank"),
-        centerTitle: true,
+      home: TelaDashboard(
+        titulo: nomeApp,
       ),
     );
   }
