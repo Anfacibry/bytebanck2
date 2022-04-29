ee69dd173de0550ee2d9aad06372bd2e58c2d106
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-04-29 10:25:04 -0300

    Adicionando class de contatos

---
+++ lib/components/contato_pessoa.dart
@@ -0,0 +1,10 @@
+class ContatoPessoa {
+  String imagePerfil;
+  String nomePessoa;
+  String numeroPessoa;
+  ContatoPessoa({
+    required this.imagePerfil,
+    required this.nomePessoa,
+    required this.numeroPessoa,
+  });
+}
