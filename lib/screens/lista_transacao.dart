b85b89574a8cc53e77488572359056a8e254bbe2
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-05-17 14:34:39 -0300

    atualizando lista_transação

--- lib/screens/lista_transacao.dart
+++ lib/screens/lista_transacao.dart
@@ -1,54 +1,72 @@
-import 'package:bytebank/components/conta_pessoa.dart';
+import 'package:bytebank/http/web_cliente.dart';
 import 'package:flutter/material.dart';
 
 import '../components/transacao.dart';
 
-class ListaTransacao extends StatelessWidget {
-  final List<Transacao> transacao = [];
-
-  ListaTransacao({Key? key}) : super(key: key);
+class ListaTransacao extends StatefulWidget {
+  const ListaTransacao({Key? key}) : super(key: key);
 
   @override
+  State<ListaTransacao> createState() => _ListaTransacaoState();
+}
+
+class _ListaTransacaoState extends State<ListaTransacao> {
+  @override
   Widget build(BuildContext context) {
-    transacao.add(
-      Transacao(
-        valor: 100,
-        contato: ContaPessoa(
-          id: 0,
-          nomeConta: "Bruno",
-          numeroConta: "1000",
-        ),
-      ),
-    );
     return Scaffold(
       appBar: AppBar(
         title: const Text('Transações'),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        itemCount: transacao.length,
-        itemBuilder: (context, index) {
-          final Transacao transaction = transacao[index];
-          return Card(
-            child: ListTile(
-              leading: const Icon(Icons.monetization_on),
-              title: Text(
-                transaction.valor.toString(),
-                style: const TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
+      body: FutureBuilder<List<Transacao>?>(
+          future: Future.delayed(const Duration(seconds: 3))
+              .then((value) => findAll()),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final List<Transacao> transacao = snapshot.data!;
+              if (transacao.isNotEmpty) {
+                return ListView.builder(
+                  itemCount: transacao.length,
+                  itemBuilder: (context, index) {
+                    final Transacao transaction = transacao[index];
+                    return Card(
+                      child: ListTile(
+                        leading: const Icon(Icons.monetization_on),
+                        title: Text(
+                          transaction.valor.toString(),
+                          style: const TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        subtitle: Text(
+                          transaction.contato!.nomeConta.toString(),
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              } else {
+                return const Center(
+                  child: Text("Lista de transferencias vazia"),
+                );
+              }
+            } else {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [
+                    CircularProgressIndicator(),
+                    Text("Carregando Transferencias"),
+                  ],
                 ),
-              ),
-              subtitle: Text(
-                transaction.contato!.nomeConta.toString(),
-                style: const TextStyle(
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
-          );
-        },
-      ),
+              );
+            }
+          }),
     );
   }
 }
